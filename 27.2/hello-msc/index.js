d632cf196f75c8672e5f6f0bb676078ec011982e
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-01 00:19:46 -0300

    Preparando middlewares da collection books

--- 27.2/hello-msc/index.js
+++ 27.2/hello-msc/index.js
@@ -1,15 +1,25 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const Author = require('./controllers/Author');
+const Book = require('./controllers/BooksController');
+const errorMiddleware = require('./middlewares/error');
+
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json());
 
 app.get('/authors', Author.getAll);
-
-app.get('/authors/:id',Author.findById);
-
+app.get('/authors/:id', Author.findById);
 app.post('/authors', Author.create);
 
-app.listen(port, () => console.log('O Pai tá ON!!!'))
+app.get('/books', Book.getAll);
+app.get('/books/:id', Book.findById);
+app.post('/books', Book.create);
+
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('O Pai tá ON!!!'))
