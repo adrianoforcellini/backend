12e3290d4040e5756fed14a9c1b1526807331a12
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-06 15:59:53 -0300

    isolando o IO do banco de dados com mongodb-memory-server para facilitar a implementaçao do stud  - 27.4 - Testes de Models - 06/07

--- 27.4/tests/models/movieModel.test.js
+++ 27.4/tests/models/movieModel.test.js
@@ -2,6 +2,8 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const MoviesModel = require('../../models/movieModel');
 const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
 
 /*
   Como ainda não temos a implementação, vamos fixar
@@ -18,21 +20,37 @@ describe('Insere um novo filme no BD', () => {
     directedBy: 'Jane Dow',
     releaseYear: 1999,
   };
-  before(() => {
-    const ID_EXAMPLE = '604cb554311d68f491ba5781';
-    const connectionMock = {
-      db: async () => ({
-        collection: async () => ({
-          insertOne: async () => ({
-            insertedId: ID_EXAMPLE,
-          })
-        })
-      })
-    };
+  // before(() => {
+  //   const ID_EXAMPLE = '604cb554311d68f491ba5781';
+  //   const connectionMock = {
+  //     db: async () => ({
+  //       collection: async () => ({
+  //         insertOne: async () => ({
+  //           insertedId: ID_EXAMPLE,
+  //         })
+  //       })
+  //     })
+  //   };
+
+  //   sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+  // });  -> isolando os teste com sinon stub ( criando uma implementação ao stub )
+
+  // agora , o mesmo procedimento, mas isolandoo IO com um banco de dados em memória ( mongodb-memory-server )
+  
+  before(async () => {
+
+    const DBServer = new MongoMemoryServer();
+    const URLMock = await DBServer.getUri();
+
+    const connectionMock = await MongoClient
+      .connect(URLMock, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
 
     sinon.stub(MongoClient, 'connect').resolves(connectionMock);
-  });
 
+  });
   // Restauraremos a função `connect` original após os testes.
   after(() => {
     MongoClient.connect.restore();
