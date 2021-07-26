2e8bf6bb9fa7139767c0d4f5ce62589c28e6af13
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-26 19:52:11 -0300

    COnceitos Solid Aplicados

--- 30.1 - SOLID e ORM/index.js
+++ 30.1 - SOLID e ORM/index.js
@@ -1,87 +1,45 @@
-const defaultPlants = [
-    {
-      id: 1,
-      breed: "Bromelia",
-      needsSun: false,
-      origin: "Argentina",
-      size: 102,
-      specialCare: {
-        waterFrequency: 3,
-      },
-    },
-    {
-      id: 2,
-      breed: "Orquidea",
-      size: 99,
-      needsSun: false,
-      origin: "Brazil",
-    },
-  ];
-  
-  let createdPlants = 0;
-  
-  const initPlant = (id, breed, needsSun, origin, specialCare, size) => {
-    const waterFrequency = needsSun ? size *  0.77 + (origin === 'Brazil' ? 8 : 7)
-      : (size / 2) *  1.33 + (origin === 'Brazil' ? 8 : 7)
-    const newPlant = {
-      id,
-      breed,
-      needsSun,
-      origin,
-      specialCare: {
-        waterFrequency,
-        ...specialCare,
-      },
-      size,
-    };
-    return newPlant;
-  };
-  
-  const savePlants = () => {
-    const plants = JSON.stringify(defaultPlants);
-    localStorage.setItem("plants", plants);
-  };
-  
-  const getPlants = () => {
-    const plants = JSON.parse(localStorage.getItem("plants"));
-    return plants;
-  };
-  
-  const getPlantById = (id) => {
-    return defaultPlants.filter((plant) => plant.id === id);
-  };
-  
-  const removePlantById = (id) => {
-    const newPlants = defaultPlants.filter((plant) => plant.id !== id);
-    localStorage.setItem("plants", JSON.stringify(newPlants));
-  };
-  
-  const getPlantsThatNeedsSunWithId = (id) => {
-    const filteredPlants = defaultPlants.filter((plant) => {
-      if (plant.needsSun && plant.id === id) {
-        if (plant.specialCare.waterFrequency > 2) {
-          return plant;
-        }
-      }
-    });
-    localStorage.setItem("plants", JSON.stringify(filteredPlants));
-    return filteredPlants;
-  };
-  
-  const editPlant = (plantId, newPlant) => {
-    return defaultPlants.map((plant) => {
-      if (plant.id === plantId) {
-        return newPlant;
-      }
-      return plant;
-    });
-  };
-  
-  const createNewPlant = (plant) => {
-    const mappedPlant = initPlant({ ...plant });
-    defaultPlants.push(mappedPlant);
-    createdPlants++;
-    localStorage.setItem("createdPlants", String(createdPlants));
-    localStorage.setItem("plants", JSON.stringify(defaultPlants));
-    return defaultPlants;
-  };
+const express = require("express");
+const app = express();
+const plantsModule = require("./plants.js");
+
+app.use(express.json());
+
+app.get("/plants", (req, res) => {
+  const plants = plantsModule.getPlants();
+  res.send(plants);
+});
+
+app.get("/plant/:id", (req, res) => {
+  const { id } = req.params;
+  const plant = plantsModule.getPlantById(id);
+  res.send(plant);
+});
+
+app.delete("/plant/:id", (req, res) => {
+  const { id } = req.params;
+  const plant = plantsModule.removePlantById(id);
+  res.send(plant);
+});
+
+app.post("/plant/:id", (req, res) => {
+  const { id } = req.params;
+  const newPlant = req.body.plant;
+  const plant = plantsModule.editPlant(id, newPlant);
+  res.send(plant);
+});
+
+app.post("/plant", (req, res) => {
+  const newPlant = req.body.plant;
+  const plant = plantsModule.createNewPlant(newPlant);
+  res.send(plant);
+});
+
+app.get("/sunny/:id", (req, res) => {
+  const { id } = req.params;
+  const plant = plantsModule.getPlantById(id);
+  res.send(plant);
+});
+
+app.listen(3001, function () {
+  console.log("Ouvindo a porta 3001!");
+});
