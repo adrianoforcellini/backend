844ec9df3339ea02f47687a732d4d18ad76dbfa8
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-11 13:10:43 -0300

    Criando um chat com Socket.io

--- 31.3 - Socket.io/refatoração_do_conteudo/sockets/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = (io) => {
-  io.on('connection', (socket) => {
-    socket.emit('ola', 'Namastê, caro visitante. :)');
-    socket.on('ping', () => {
-      console.log(`${socket.id} emitiu um ping!`);
-      io.emit('pong', `Já chegou o Ping voooadoooorrr!!`);
-    });
-  });
-};
---
+++ 31.3 - Socket.io/sochat.io/sockets/ping.js
@@ -0,0 +1,9 @@
+// module.exports = (io) => {
+//   io.on('connection', (socket) => {
+//     socket.emit('ola', 'Namastê, caro visitante. :)');
+//     socket.on('ping', () => {
+//       console.log(`${socket.id} emitiu um ping!`);
+//       io.emit('pong', `Já chegou o Ping voooadoooorrr!!`);
+//     });
+//   });
+// };
