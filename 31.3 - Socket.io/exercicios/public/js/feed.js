c4aca881f3a5c0948dd275114af88775a1b9ff27
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-12 11:04:37 -0300

    ex 5 - Faça com que o ícone com id starIcon emita um evento starPost e atualize a quantidade diretamente pelo front-end para o cliente atual.

--- 31.3 - Socket.io/exercicios/public/js/feed.js
+++ 31.3 - Socket.io/exercicios/public/js/feed.js
@@ -3,3 +3,16 @@ const socket = window.io();
 document.querySelector('#likeIcon').addEventListener('click', () => {
   socket.emit('likePost');
 });
+
+socket.on('updateLikes', (countLikes) => {
+  document.querySelector('#currentLikes').innerHTML = countLikes;
+});
+
+document.querySelector('#starIcon').addEventListener('click', () => {
+  const currentStarsElement = document.querySelector('#currentStars');
+  let currentStars = parseInt(currentStarsElement.innerHTML);
+  
+  currentStarsElement.innerHTML = currentStars + 1;
+
+  socket.emit('starPost');
+});
